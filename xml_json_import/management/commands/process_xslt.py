c9ced5612bf382b9c5304276c608c25bc2f95f49
Lev Veshnyakov <lev@lev.website>
2016-01-31 20:49:50 +0300

    Add validation against Relax NG schema in management command "process_xslt"
    Add 2 more options: --validate and --rng_file. It's now posible to transform
    external source via XSLT and validate it aganst Relax NG schema to prove is
    the XSLT transformation well done to start importing to the database

--- xml_json_import/management/commands/process_xslt.py
+++ xml_json_import/management/commands/process_xslt.py
@@ -1,13 +1,20 @@
 from django.core.management.base import BaseCommand
 from lxml import etree, html
 import urllib2
+from os import path
 
 class Command(BaseCommand):
     help = 'Processes XSLT transformation on a fetched by URL resource and outputs the result'
 
     def add_arguments(self, parser):
         parser.add_argument('url', help='URL to fetch source XML')
-        parser.add_argument('xslt_file', help='path to XSLT transformation file')
+        parser.add_argument('xslt_file', help='Path to XSLT transformation file')
+        parser.add_argument('--validate', action='store_true', 
+                             help='Validate against Relax NG schema after transformation')
+        rng_file = path.join(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))), 'schema.rng')
+        parser.add_argument('--rng_file', default=rng_file,
+                             help='Path to RELAX NG file. Defaults to schema.rng in module dir. '
+                                  'Used only if --validate is set')
 
     def handle(self, *args, **options):
         response = urllib2.urlopen(options['url'])
@@ -19,5 +26,14 @@ class Command(BaseCommand):
             source_etree = html.parse(response)
         xslt_etree = etree.parse(options['xslt_file'])
         transform = etree.XSLT(xslt_etree)
-        output = etree.tostring(transform(source_etree), pretty_print=True, encoding=encoding)
+        transformed_etree = transform(source_etree)
+        output = etree.tostring(transformed_etree, pretty_print=True, encoding=encoding)
         print '<?xml version="1.0" encoding="' + encoding + '"?>\n' + output
+        if options['validate']:
+            rng_file_etree = etree.parse(options['rng_file'])
+            relaxng = etree.RelaxNG(rng_file_etree)
+            try:
+                relaxng.assertValid(transformed_etree)
+                print 'Document is valid'
+            except etree.DocumentInvalid as ex:
+                print 'Document is not valid: ' + str(ex)
