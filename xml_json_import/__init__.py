3509243e467a8546a3fa9ba123f77a1a96643402
Lev Veshnyakov <lev@lev.website>
2016-01-31 09:09:08 +0300

    Throw exception for not existing XSLT_FILES_DIR setting

--- xml_json_import/__init__.py
+++ xml_json_import/__init__.py
@@ -3,3 +3,5 @@ from django.conf import settings
 class XmlJsonImportModuleException(Exception):
     pass
 
+if not hasattr(settings, 'XSLT_FILES_DIR'):
+    raise XmlJsonImportModuleException('Settings must contain XSLT_FILES_DIR parameter')
