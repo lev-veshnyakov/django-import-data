2facb0c8794c9529ccb17631a90b0ee181c4eb5b
Lev Veshnyakov <lev@lev.website>
2016-01-31 13:01:31 +0300

    Add exception handling for invalid XSLT files

--- xml_json_import/__init__.py
+++ xml_json_import/__init__.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from os import path
+from os import path, listdir
+from lxml import etree
 
 class XmlJsonImportModuleException(Exception):
     pass
@@ -9,3 +10,15 @@ if not hasattr(settings, 'XSLT_FILES_DIR'):
 
 if not path.exists(settings.XSLT_FILES_DIR):
     raise XmlJsonImportModuleException('Directory specified by XSLT_FILES_DIR does not exist')
+
+for filename in listdir(settings.XSLT_FILES_DIR):
+    filepath = path.join(settings.XSLT_FILES_DIR, filename)
+    if path.isfile(filepath):
+        try:
+            xslt_etree = etree.parse(filepath)
+        except etree.XMLSyntaxError as er:
+            raise XmlJsonImportModuleException('File ' + filepath + ' is not a valid XML file: ' + str(er))
+        try:
+            transform = etree.XSLT(xslt_etree)
+        except etree.XSLTParseError as er:
+            raise XmlJsonImportModuleException('File ' + filepath + ' is not a valid XSLT file: ' + str(er))
