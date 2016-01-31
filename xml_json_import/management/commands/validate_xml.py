8e2a6a00fb815b74dac99b76b4003a5ff941a60e
Lev Veshnyakov <lev@lev.website>
2016-01-31 16:51:18 +0300

    Add management command "validate_xml"
    It validates given XML file with given RELAX NG schema. Used while development

---
+++ xml_json_import/management/commands/validate_xml.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from lxml import etree
+
+class Command(BaseCommand):
+    help = 'Validates given XML file with RELAX NG schema'
+
+    def add_arguments(self, parser):
+        parser.add_argument('xml_file', help='path to XML file')
+        parser.add_argument('rng_file', help='path to RELAX NG file')
+
+    def handle(self, *args, **options):
+        xml_file_etree = etree.parse(options['xml_file'])
+        rng_file_etree = etree.parse(options['rng_file'])
+        relaxng = etree.RelaxNG(rng_file_etree)
+        try:
+            relaxng.assertValid(xml_file_etree)
+            self.stdout.write('Document is valid')
+        except etree.DocumentInvalid as ex:
+            self.stdout.write('Document is not valid: ' + str(ex))
