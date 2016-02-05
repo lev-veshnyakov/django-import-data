74571960b390a775f873a6ebfcb5bdaaf4030545
Lev Veshnyakov <lev@lev.website>
2016-02-06 02:48:47 +0300

    Add console command json_to_xml for printing JSON converted to XML It helps to write XSLT tarnsformation files for JSON sources

---
+++ import_data/management/commands/json_to_xml.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+import process_xslt
+
+class Command(BaseCommand):
+    help = 'Converts JSON file to XML and prints the output'
+
+    def add_arguments(self, parser):
+        parser.add_argument('url', help='URL to fetch source XML')
+
+    def handle(self, *args, **options):
+        etree, encoding = process_xslt.load_source_by_url(options['url'])
+        process_xslt.print_xml(etree, encoding)
